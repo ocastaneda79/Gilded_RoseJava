dda97802b5a966d676e9e65e42644293054819ca
ocastaneda79 <occallejon@gmail.com>
2020-10-26 23:27:20 +0100

    Refactor tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+    private Item[] GetItems() {
+        return new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10,  10),
+                new Item("Foo",  0, 10),
+                new Item("Foo",  0, 0)};
+    }
+    private  Item[] SutItems = GetItems();
+
 
     @Test
     void WhenUpdatingSulfurasThenRemaisEqual() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10,  10) };
+
         //Arrange
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(SutItems);
         //Act
         app.updateQuality();
         //Assert
@@ -20,24 +27,24 @@ class GildedRoseTest {
     }
     @Test
     void WhenUpdatingNormalItemIfSellInhasPassedThenQualityDegradesTwiceAsFast() {
-        Item[] items = new Item[] { new Item("Foo",  0, 10) };
+
         //Arrange
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(SutItems);
         //Act
         app.updateQuality();
         //Assert
-        assertEquals(-1, app.items[0].sellIn);
-        assertEquals(8, app.items[0].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(8, app.items[1].quality);
     }
     @Test
     void WhenUpdatingNormalItemWithZeroQualityThenQualityRemainsZero() {
-        Item[] items = new Item[] { new Item("Foo",  0, 0) };
+
         //Arrange
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(SutItems);
         //Act
         app.updateQuality();
         //Assert
-        assertEquals(-1, app.items[0].sellIn);
-        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(0, app.items[2].quality);
     }
 }
